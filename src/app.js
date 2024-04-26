da0388f817af7dc65624ac1905f1d2ecf496e144
gespana26 <151874827+gespana26@users.noreply.github.com>
2024-04-26 23:41:46 +0000

    Entrega_26abr2014

--- src/app.js
+++ src/app.js
@@ -6,6 +6,80 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cart = new Carta();
 };
+//write your code here
+class Carta {
+  // simbolos;
+  // valores;
+  // valor;
+  // simbol;
+  constructor() {
+    const simbolos = ["corazon", "diamante", "pica", "trebol"];
+    const valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let carta = this.seleccionNumero(simbolos, valores);
+    this.asignarColor(this.simbol);
+    this.asignarSimbolo(this.simbol);
+    this.asignarValor(this.valor);
+  }
+  seleccionNumero(simbolos, valores) {
+    this.simbolos = simbolos;
+    this.valores = valores;
+    this.simbol = Math.floor(Math.random() * this.simbolos.length);
+    this.valor = Math.floor(Math.random() * this.valores.length);
+    return this.valor, this.simbol;
+  }
+  asignarColor(simbol) {
+    if (
+      this.simbolos[simbol] === "corazon" ||
+      this.simbolos[simbol] === "diamante"
+    ) {
+      simboloArriba.style.color = "red";
+      numero.style.color = "red";
+      simboloAbajo.style.color = "red";
+    } else {
+      simboloArriba.style.color = "black";
+      numero.style.color = "black";
+      simboloAbajo.style.color = "black";
+    }
+  }
+
+  asignarSimbolo(simbol) {
+    switch (this.simbolos[simbol]) {
+      case "corazon":
+        simboloArriba.innerHTML = "♥";
+        simboloAbajo.innerHTML = "♥";
+        break;
+      case "diamante":
+        simboloArriba.innerHTML = "♦";
+        simboloAbajo.innerHTML = "♦";
+        break;
+      case "trebol":
+        simboloArriba.innerHTML = "♣";
+        simboloAbajo.innerHTML = "♣";
+        break;
+      default:
+        simboloArriba.innerHTML = "♠";
+        simboloAbajo.innerHTML = "♠";
+    }
+  }
+  asignarValor(valor) {
+    console.log("entro a asignar valor", this.valores[valor]);
+    numero.innerHTML = this.valores[valor];
+  }
+}
